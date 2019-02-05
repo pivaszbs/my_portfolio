7bef9005e84109b832bc3d43c01943fbaf306235
pivaszbs <pivaszbs@github.com>
2019-02-05 19:22:27 +0300

    bootstrap, likely

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import './app.css';
+
 import MainInfo from '../main-info/main-info';
 import Profile from '../profile/profile';
 import Contact from '../contact/contact';
@@ -7,11 +9,20 @@ import Skills from '../skills/skills';
 import Experience from '../expirience/experience';
 import Software from '../software/software';
 import Education from '../education/education';
+
+
+
 const App = () => {
+
+    const maininfo = {
+        name: "Anton",
+        surname: "Krylov",
+        workplace: "Front-end"
+    };
     return (
         <div className="container">
-            <div className="flex-side">
-                <MainInfo />
+            <div className="col-lg-6 col-xs-12">
+                <MainInfo {...maininfo} />
                 <Profile />
                 <Contact />
                 <Skills />
--- src/components/main-info/main-info.js
+++ src/components/main-info/main-info.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './main-info.css';
 import MyFoto from './ez.jpg';
 
-const MainInfo = ({name, surname, workplace}) => {
-   return (
-       <div className="main-img">
-            <div className="circle">
-                <img alt="my huy" className="main-img_photo" src={MyFoto} />
-            </div>
-
-       </div>
-  );
+const MainInfo = ({ name, surname, workplace }) => {
+    return (
+        <div className="main-info">
+                <div className="main-img">
+                    <div className="circle">
+                        <img alt="my huy" className="main-img_photo" src={MyFoto} />
+                    </div>
+                </div>
+                <div className="main-text">
+                    <h2>{name}</h2>
+                    <h1>{surname}</h1>
+                    <h3>{workplace}</h3>
+                </div>
+        </div>
+    );
 };
 
 export default MainInfo;
