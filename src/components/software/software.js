1d211a9cd55c7c1d2c2384efa2e5e29d7a9f9b21
Picroc <kingofrootelement@gmail.com>
2019-02-05 23:31:21 +0300

    Added soft

--- src/components/software/software.js
+++ src/components/software/software.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './software.css';
 
 
-const Software = () => {
+const Software = ({sofInfo}) => {
+
+   const elements = sofInfo.map((item) => {
+
+      const {tech, perc} = item;
+
+      return (
+         <div className="sw-item">
+            <p>{tech}</p>
+            <div className="p-bar">
+               <div className="p-line" style={{width: perc}}></div>
+            </div>
+         </div>
+      );
+   });
+
    return (
-      <div>
-
-
-       </div>
-  );
+      <div className="d-flex sw-group">
+         {elements}
+      </div>
+   );
 };
 
 export default Software;
