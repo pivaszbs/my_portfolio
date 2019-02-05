d2ee7379e4a3b88ae316cdbd8c7165997056c3bb
Picroc <kingofrootelement@gmail.com>
2019-02-05 22:48:28 +0300

    Added experience

--- src/components/expirience/experience.js
+++ src/components/expirience/experience.js
@@ -2,12 +2,25 @@ import React from 'react';
 import './experience.css';
 
 
-const Experience = () => {
-   return (
-      <div>
+const Experience = ({expInfo}) => {
+   const elements = expInfo.map((item) => {
+      const { id, company = 'lorem', job = 'lorem', info } = item;
 
-
-       </div>
+      return (
+          <span key={id}>
+              <h3 style={{ color: 'steelblue' }}>
+                  <i style={{ color: 'black' }} className='fa fa-circle' />  {company.toUpperCase()}
+                  //2007-2019
+              </h3>
+              <h4>{job.toUpperCase()}</h4>
+              <p>{info}</p>
+          </span>
+      );
+  });
+  return (
+      <div className='edu-group-item'>
+          {elements}
+      </div>
   );
 };
 
